c78ef73476f4145e42ec30fcce30f4bc0a87f568
PavlovskayaVA <114114177+PavlovskayaVA@users.noreply.github.com>
2023-05-01 12:35:22 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,1 @@
-document.querySelector('.header__burger').addEventListener('click', function() {
-    this.classList.toggle('active');
-    document.querySelector('.header__nav').classList.toggle('open');
-})
+
